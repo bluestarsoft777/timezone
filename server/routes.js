e84755162418d9f2b1d1b3d57f5a6d9457f2d068
kgmsoft <princehome0505@gmail.com>
2015-06-07 23:31:19 +0200

    disabled extra routing

--- server/routes.js
+++ server/routes.js
@@ -12,12 +12,12 @@ module.exports = function(app) {
   app.use('/api/locations', require('./api/locations'));
 
   // All undefined asset or api routes should return a 404
-  app.route('/:url(api|auth|components|app|bower_components|assets)/*')
-   .get(errors[404]);
+  // app.route('/:url(api|auth|components|app|bower_components|assets)/*')
+  //  .get(errors[404]);
 
   // All other routes should redirect to the index.html
-  app.route('/*')
-    .get(function(req, res) {
-      res.sendFile(app.get('appPath') + '/index.html');
-    });
+  // app.route('/*')
+  //   .get(function(req, res) {
+  //     res.sendFile(app.get('appPath') + '/index.html');
+  //   });
 };
